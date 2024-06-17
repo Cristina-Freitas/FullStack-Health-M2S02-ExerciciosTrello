eea849eed3dc48d7976956ba3f0284fc0051bc1e
CrisTinaFLN <crisjf.tina@hotmail.com>
2024-06-17 03:47:40 -0300

    Ex4: Telas do console

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,6 +1,89 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        //  Ex4: Telas do console
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
+
+        System.out.println();
+        while (true) {
+            System.out.println("""
+                    Escolha uma opção:
+                    1 - Cadastrar paciente
+                    2 - Listar pacientes
+                    3 - Alterar informações do paciente
+                    4 - Mostrar informações do paciente
+                    5 - Registrar atividade física para um paciente
+                    6 - Remover paciente
+                    7 - Sair""");
+           
+            opcao = Integer.parseInt(scanner.next());
+
+            switch (opcao) {
+                case 1:
+                    System.out.println();
+                    System.out.println("Cadastrar paciente");
+                    telaCadastrarPaciente(scanner);
+                    continue;
+                case 2:
+                    System.out.println();
+                    System.out.println("Lista de pacientes");
+                    telaListarPacientes();
+                    continue;
+                case 3:
+                    System.out.println();
+                    System.out.println("Alterar informações do paciente");
+                    telaAlterarPaciente(scanner);
+                    continue;
+                case 4:
+                    System.out.println();
+                    System.out.println("Mostrar informações do paciente");
+                    telaExibirPaciente(scanner);
+                    continue;
+                case 5:
+                    System.out.println();
+                    System.out.println("Registrar atividade física para um paciente");
+                    telaRegistrarAtividade(scanner);
+                    continue;
+                case 6:
+                    System.out.println();
+                    System.out.println("Remover paciente");
+                    telaRemoverPaciente(scanner);
+                    continue;
+                case 7:
+                    System.out.println("Saindo..");
+                    break;
+                default:
+                    continue;
+            }
+            break; // break referente ao while;
+        }
+        scanner.close();
     }
+
+    private static void telaCadastrarPaciente(Scanner scanner) {
+    }
+
+    private static void telaListarPacientes() {
+    }
+
+    private static void telaAlterarPaciente(Scanner scanner) {
+        
+    }
+
+    private static void telaExibirPaciente(Scanner scanner) {
+        
+    }
+
+    private static void telaRegistrarAtividade(Scanner scanner) {
+        
+    }
+
+    private static void telaRemoverPaciente(Scanner scanner) {
+        
+    }
+
+
 }
