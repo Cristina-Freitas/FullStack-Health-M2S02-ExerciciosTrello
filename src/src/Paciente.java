e35b95ea5175acd99d83dd75f6d10c48a2dbeffb
CrisTinaFLN <crisjf.tina@hotmail.com>
2024-06-17 03:24:50 -0300

    Ex3: Criação dos métodos

--- src/src/Paciente.java
+++ src/src/Paciente.java
@@ -14,4 +14,28 @@ public class Paciente {
     public int frequenciaCardiaca;
     public String dietaAlimentar;
     public List<String> atividadesFisicas = new ArrayList<>();
+
+    // Ex3: criação dos métodos: Cálculo de IMC | Monitoramento de Paciente | Registro de Atividades Físicas
+
+    public double calcularIMC() {
+        return this.peso / (this.altura * this.altura);
+    }
+
+    public void monitoramento() {
+        System.out.println("Informações do paciente:\n" +
+                "Nome: " + this.nome + "\n" +
+                "Idade: " + this.idade + "\n" +
+                "Peso: " + this.peso + "\n" +
+                "Altura: " + this.altura + "\n" +
+                "Pressão arterial: " + this.pressaoArterial + " mmHg." + "\n" +
+                "Frequência cardíaca: " + this.frequenciaCardiaca + " bpm." + "\n" +
+                "Dieta alimentar: " + this.dietaAlimentar + "\n" +
+                "Atividades físicas: " + this.atividadesFisicas + "\n" +
+                "IMC: " + this.calcularIMC());
+    }
+
+    public void registrarAtividadeFisica(String novaAtividade) {
+
+               this.atividadesFisicas.add(novaAtividade);
+    }
 }
