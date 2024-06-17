772c6750df875f13ef0c2b618d5c8a77880310c1
CrisTinaFLN <crisjf.tina@hotmail.com>
2024-06-17 02:46:17 -0300

    Ex1: Classe Paciente

---
+++ src/src/Paciente.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Paciente {
+
+    //Ex1: crie a Classe Paciente
+
+    // Atributos
+    public String nome;
+    public int idade;
+    public double peso;
+    public double altura;
+    public int pressaoArterial;
+    public int frequenciaCardiaca;
+    public String dietaAlimentar;
+    public List<String> atividadesFisicas = new ArrayList<>();
+}
